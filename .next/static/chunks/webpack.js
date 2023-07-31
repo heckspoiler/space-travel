4f858f9368018cd62f9be19fba2ed5e04252ecc5
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-07-31 19:43:52 +0200

    adding navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1705ef210918c86d"; }
+/******/ 		__webpack_require__.h = function() { return "079a2914bf694f35"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,24 @@
+import Link from "next/link";
+
 function Header() {
   return (
     <header className="bg-transparent flex flex-row items-center p-4 w-screen">
       <img src="/assets/shared/logo.svg" alt="logo" className="h-12" />
       <div className="h-1px w-72 bg-gray-400"></div>
+      <nav>
+        <Link href="/" className="text-white">
+          HOME
+        </Link>
+        <Link href="/destination" className="text-white">
+          DESTINATION
+        </Link>
+        <Link href="/crew" className="text-white">
+          CREW
+        </Link>
+        <Link href="/technology" className="text-white">
+          TECHNOLOGY
+        </Link>
+      </nav>
     </header>
   );
 }
