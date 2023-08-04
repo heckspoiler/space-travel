a9aa2bc68adadbc815bd3f9b9d60b0ad34141493
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-08-04 23:22:18 +0200

    crew section tablet responsiveness finished

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "49271ac779eed53c"; }
+/******/ 		__webpack_require__.h = function() { return "419aac74e7a235d3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
